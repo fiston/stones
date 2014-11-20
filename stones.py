d37998816521f087acfc2b696cef0d7963ea17f4
erwagasore <rwagasore@gmail.com>
2014-11-20 09:56:37 +0200

    added counter view

--- stones.py
+++ stones.py
@@ -10,13 +10,26 @@ application = Flask(__name__)
 def index():
     return "<h1>Hello, World!</h1>"
 
+
 @application.route('/kigali')
 def where_are_you():
     return "<h2>I am in the capital of Rwanda</h2>"
 
+
 @application.route('/user/<name>')
 def get_user(name):
     return "<h2>Hello, %s</h2>" % name
+
+
+@application.route('/greet/<name>')
+def greetings(name):
+    return "<h2>Mwaramutse, %s. Mbese murugo baraho!</h2>" % name
+
+
+@application.route('/counter/<int:value>')
+def counter(value):
+    values = {1: 'one', 2:'two', 3:'three', 4:'four', 5:'five'}
+    return "Number %s" % values.get(value, 'Unknown')
 
 if __name__ == '__main__':
     application.run(port=3000, debug=True)
