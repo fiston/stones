8f87b89ce728459afb74f359e2a0093ad0aa0fb3
erwagasore <rwagasore@gmail.com>
2014-11-26 20:20:47 +0200

    integrated flask-script, reword variables & files

---
+++ app.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask.ext.script import Manager
+from flask import Flask, render_template
+
+app = Flask(__name__)
+manager = Manager(app)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    manager.run()
