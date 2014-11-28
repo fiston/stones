c31304598f9995f433e9a9dac3426fb61c5137d3
erwagasore <rwagasore@gmail.com>
2014-11-28 13:00:58 +0200

    added flask bootstrap extension

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask.ext.script import Manager
+from flask_script import Manager
+from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     manager.run()
