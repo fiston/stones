74ffd0557880601e62a5fe4fe90517b542192f0b
njclaude <jclaudendahiro@gmail.com>
2014-12-01 09:50:41 +0200

    404%500 customization

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask_script import Manager
+# from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask import Flask, render_template
 
 app = Flask(__name__)
-manager = Manager(app)
+# manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 
@@ -14,6 +14,19 @@ bootstrap = Bootstrap(app)
 def home():
     return render_template('home.html')
 
+@app.route('/base')
+def base():
+    return render_template('base.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 400
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
