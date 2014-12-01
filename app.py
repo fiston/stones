0e46260c90cabb5bae2c8c3055976638dc52c8a5
bfabrice <bafabros@yahoo.fr>
2014-12-01 10:57:20 +0200

    this is my application with error handling pages

--- app.py
+++ app.py
@@ -14,6 +14,14 @@ bootstrap = Bootstrap(app)
 def home():
     return render_template('home.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
     manager.run()
