55384a9292706ce14e15734c0751b9e4bc0f2ee9
rdidace2 <gaceyeyekarambi@yahoo.com>
2014-12-01 10:34:33 +0200

    this is the branch commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -14,6 +15,15 @@ bootstrap = Bootstrap(app)
 def home():
     return render_template('home.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
+    # manager.run()
