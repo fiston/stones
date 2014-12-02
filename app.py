5a78b40dd360f0bbb99638bbcfc88f544bf35225
tvenant <ventum11@yahoo.fr>
2014-12-02 11:31:04 +0200

    updating links using url_for function

--- app.py
+++ app.py
@@ -15,6 +15,19 @@ bootstrap = Bootstrap(app)
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/service')
+def service():
+    return render_template('service.html')
+
 @app.errorhandler(404)
 def error_404(e):
     return render_template('404.html'), 404
