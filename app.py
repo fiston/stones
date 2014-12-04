37b9019d03ddca49230fc6c6b08867e48ebae585
erwagasore <rwagasore@gmail.com>
2014-12-04 13:35:36 +0200

    Changed the template to benefit from base

--- app.py
+++ app.py
@@ -3,16 +3,32 @@
 
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'aabbccddeeff!##$##@'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('Name', description="Please enter your name",
+                       validators=[DataRequired()])
+    submit = SubmitField('Send')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    return render_template('home.html')
+    name = None
+    form = NameForm()
+
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('home.html', form=form, name=name)
 
 
 if __name__ == '__main__':
