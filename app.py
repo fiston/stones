aed275aa36bd0362eb1935378ab89830d9cb8f5e
erwagasore <rwagasore@gmail.com>
2014-12-08 11:59:32 +0200

    Added flash messages

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask_bootstrap import Bootstrap
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
-from flask import Flask, render_template
+from flask import Flask, render_template, session, flash
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'aabbccddeeff!##$##@'
@@ -15,21 +15,25 @@ bootstrap = Bootstrap(app)
 
 
 class NameForm(Form):
-    name = StringField('Name', description="Please enter your name",
+    name = StringField('Name',
+                       description="Please enter your name",
                        validators=[DataRequired()])
     submit = SubmitField('Send')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    name = None
     form = NameForm()
-
     if form.validate_on_submit():
-        name = form.name.data
+        session['name'] = form.name.data
         form.name.data = ''
-    return render_template('home.html', form=form, name=name)
+        flash("Something is wrong", 'alert-danger')
+    return render_template('home.html', form=form)
 
+
+@app.route('/about/stones')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     manager.run()
